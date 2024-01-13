33354200e06efa444f714f8138f9f324b18dbfe1
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-14 01:36:07 +0900

    feat : Advice 종류 추가

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -1,7 +1,6 @@
 package hello.aop;
 
-import hello.aop.order.aop.AspectV4Pointcut;
-import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,7 +10,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopApplication {
 
 	public static void main(String[] args) {

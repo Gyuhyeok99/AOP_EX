5d3cc97670704f34c7203c0b8055c7276f027ca7
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-19 14:14:35 +0900

    feat : CallSErviceV2 추가

---
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,28 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * ObjectProvider(Provider), ApplicationContext를 사용해서 지연(LAZY) 조회
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callSErviceV2 = callServiceProvider.getObject();
+        callSErviceV2.internal();// 외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
